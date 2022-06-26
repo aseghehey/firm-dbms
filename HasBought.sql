c8e429aa182b01be40c3fbfb108e0cd3f0660fdd
easeghehey <98903704+easeghehey@users.noreply.github.com>
2022-06-26 15:05:36 -0400

    hasb

---
+++ HasBought.sql
@@ -0,0 +1,26 @@
+create table HasBought (
+	ClientID INT,
+	InvestmentID INT,
+	Price DECIMAL(7,2),
+	DateBought DATE
+);
+insert into HasBought (ClientID, InvestmentID, Price, DateBought) values (437, 588, 40625.46, '2022-01-10');
+insert into HasBought (ClientID, InvestmentID, Price, DateBought) values (432, 592, 47692.2, '2021-02-12');
+insert into HasBought (ClientID, InvestmentID, Price, DateBought) values (449, 500, 16534.7, '2021-04-17');
+insert into HasBought (ClientID, InvestmentID, Price, DateBought) values (482, 587, 32020.79, '2022-03-29');
+insert into HasBought (ClientID, InvestmentID, Price, DateBought) values (483, 548, 46123.58, '2021-10-17');
+insert into HasBought (ClientID, InvestmentID, Price, DateBought) values (446, 535, 52145.12, '2021-05-30');
+insert into HasBought (ClientID, InvestmentID, Price, DateBought) values (499, 589, 49796.87, '2021-09-21');
+insert into HasBought (ClientID, InvestmentID, Price, DateBought) values (454, 500, 49911.67, '2020-10-15');
+insert into HasBought (ClientID, InvestmentID, Price, DateBought) values (440, 573, 47124.88, '2021-08-14');
+insert into HasBought (ClientID, InvestmentID, Price, DateBought) values (465, 509, 19018.23, '2021-04-27');
+insert into HasBought (ClientID, InvestmentID, Price, DateBought) values (479, 593, 20851.36, '2021-05-05');
+insert into HasBought (ClientID, InvestmentID, Price, DateBought) values (424, 579, 64617.82, '2021-12-30');
+insert into HasBought (ClientID, InvestmentID, Price, DateBought) values (401, 535, 21775.52, '2022-02-17');
+insert into HasBought (ClientID, InvestmentID, Price, DateBought) values (419, 588, 62679.5, '2020-09-18');
+insert into HasBought (ClientID, InvestmentID, Price, DateBought) values (467, 512, 69972.64, '2021-11-18');
+insert into HasBought (ClientID, InvestmentID, Price, DateBought) values (440, 534, 58739.23, '2021-06-24');
+insert into HasBought (ClientID, InvestmentID, Price, DateBought) values (455, 567, 53457.68, '2022-02-08');
+insert into HasBought (ClientID, InvestmentID, Price, DateBought) values (463, 510, 58899.81, '2020-08-24');
+insert into HasBought (ClientID, InvestmentID, Price, DateBought) values (460, 580, 38634.48, '2021-05-28');
+insert into HasBought (ClientID, InvestmentID, Price, DateBought) values (472, 544, 65431.65, '2021-09-01');
