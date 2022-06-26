e2f6c876b84c6026a0209fc1125427f6b12dee58
easeghehey <98903704+easeghehey@users.noreply.github.com>
2022-06-26 15:11:24 -0400

    Update Brokers.sql

--- Brokers.sql
+++ Brokers.sql
@@ -1,33 +1,24 @@
 create table Brokers (
 	EID INT,
-	FirstName VARCHAR(50),
-	LastName VARCHAR(50),
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
 	Password VARCHAR(50),
+	StartDate DATE,
 	Salary DECIMAL(7,2),
 	Branch INT
 );
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (552, 'Christoph', 'Handyside', 'rcPI9rTr', 63056.49, 639);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (385, 'Hendrika', 'Motion', 'SFYpkkQX', 37099.07, 639);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (486, 'Gardiner', 'Bowley', '5E4tKzgyh', 26484.88, 650);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (507, 'Kaila', 'Bellanger', '4DKd8LPO7Xp', 45346.37, 632);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (412, 'Merla', 'Succamore', 'HRxQW5pGy', 23607.59, 620);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (608, 'Betta', 'Gerhts', 'nLFFa9KLJyC', 62510.4, 644);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (646, 'Laurene', 'Seagood', '6bNcPgPXDQ2n', 23989.46, 662);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (239, 'Brittany', 'Tonkinson', 'yIff6rL7Xea', 34327.36, 624);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (686, 'Corette', 'Southcoat', 'RArF87L', 72358.09, 618);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (123, 'Meara', 'Marlow', 'VE4hhLNT9', 65707.19, 658);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (328, 'Griselda', 'Cuttell', 'HkPfJpwXwDP', 14127.31, 613);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (512, 'Berkley', 'Blue', 'tWizZ8', 72377.59, 642);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (259, 'Clem', 'Giroldo', 'K7Bsh3ETCg', 54367.17, 646);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (159, 'Millicent', 'MacPadene', 'Ij4hsYE', 72765.13, 631);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (220, 'May', 'Grishelyov', 'ydVnylMrXT6Q', 56234.47, 660);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (384, 'Chaunce', 'Robjant', 'bk2XwrEARn', 14852.64, 626);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (240, 'Gilberto', 'Farthing', 'R8Prykm', 65335.8, 612);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (486, 'Rafferty', 'Chaundy', 'GI2F96', 58674.78, 645);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (118, 'Tatiania', 'Toller', 'HLMFCVr', 69102.97, 690);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (488, 'Salaidh', 'Edwardson', 'SKwchx', 42544.57, 661);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (697, 'Retha', 'Baldoni', 'W4Bbkn', 52065.24, 612);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (322, 'Sholom', 'McGlade', 'Zx83Keoe', 31722.51, 648);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (361, 'Maxy', 'Byford', 'qdvb3D14', 32552.77, 627);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (677, 'Carter', 'Roakes', '9mt0kc', 49943.54, 602);
-insert into Brokers (EID, FirstName, LastName, Password, Salary, Branch) values (274, 'Dalli', 'Slegg', 'XPsSwDas1Jez', 35328.36, 650);
+insert into Brokers (EID, first_name, last_name, Password, StartDate, Salary, Branch) values (534, 'Kym', 'Pallister', 'KFvOoesvcpVR', '2018-07-02', 55211.96, 404);
+insert into Brokers (EID, first_name, last_name, Password, StartDate, Salary, Branch) values (565, 'Isidoro', 'Monard', 'ovfXYn', '2020-11-16', 24608.72, 419);
+insert into Brokers (EID, first_name, last_name, Password, StartDate, Salary, Branch) values (673, 'Jephthah', 'Barkly', '0cWUOezmA70C', '2018-10-22', 40199.22, 427);
+insert into Brokers (EID, first_name, last_name, Password, StartDate, Salary, Branch) values (691, 'Romain', 'Calway', 'sNkGQJ', '2020-10-14', 61055.04, 426);
+insert into Brokers (EID, first_name, last_name, Password, StartDate, Salary, Branch) values (526, 'Yolanthe', 'Wrixon', 'ibEaGvG', '2020-08-21', 15945.98, 425);
+insert into Brokers (EID, first_name, last_name, Password, StartDate, Salary, Branch) values (597, 'Benson', 'Simnel', 'H0bzq5B3Hc5M', '2018-06-21', 49222.7, 405);
+insert into Brokers (EID, first_name, last_name, Password, StartDate, Salary, Branch) values (688, 'Fernanda', 'Piniur', 'YhOiET', '2019-05-20', 45802.94, 452);
+insert into Brokers (EID, first_name, last_name, Password, StartDate, Salary, Branch) values (586, 'Genevieve', 'Yon', 'uPdFABZR4Ld', '2019-05-14', 22033.29, 480);
+insert into Brokers (EID, first_name, last_name, Password, StartDate, Salary, Branch) values (689, 'Rowland', 'Calbrathe', '5wNlSJk', '2019-10-08', 64070.87, 464);
+insert into Brokers (EID, first_name, last_name, Password, StartDate, Salary, Branch) values (624, 'Alis', 'Lintill', 'W0qEP1mCyfrO', '2018-05-13', 32734.99, 482);
+insert into Brokers (EID, first_name, last_name, Password, StartDate, Salary, Branch) values (685, 'Peirce', 'Saxton', 'mJFc05', '2018-06-26', 20816.03, 443);
+insert into Brokers (EID, first_name, last_name, Password, StartDate, Salary, Branch) values (566, 'Rebecca', 'Longmire', 'UwfbqAq', '2018-10-17', 22527.58, 413);
+insert into Brokers (EID, first_name, last_name, Password, StartDate, Salary, Branch) values (678, 'Andris', 'Perree', '4tW2ks', '2020-09-26', 31445.86, 480);
+insert into Brokers (EID, first_name, last_name, Password, StartDate, Salary, Branch) values (541, 'Ruthi', 'Henlon', 'jOHO9xBa1O', '2019-02-22', 51623.32, 483);
+insert into Brokers (EID, first_name, last_name, Password, StartDate, Salary, Branch) values (668, 'Alana', 'Gerdts', '3VaHHdx', '2020-07-28', 18291.41, 410);
