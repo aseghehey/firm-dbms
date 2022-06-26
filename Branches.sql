69b988f04513af377a86496260ade98572a70a8f
easeghehey <98903704+easeghehey@users.noreply.github.com>
2022-06-26 15:12:07 -0400

    branches

---
+++ Branches.sql
@@ -0,0 +1,11 @@
+create table Branches (
+	BranchID INT,
+	Location VARCHAR(50),
+	Manager INT
+);
+insert into Branches (BranchID, Location, Manager) values (457, '0 Debra Avenue', 846);
+insert into Branches (BranchID, Location, Manager) values (465, '292 Longview Trail', 836);
+insert into Branches (BranchID, Location, Manager) values (416, '8 Carioca Junction', 830);
+insert into Branches (BranchID, Location, Manager) values (495, '0 Jenna Court', 824);
+insert into Branches (BranchID, Location, Manager) values (424, '225 Commercial Alley', 883);
+insert into Branches (BranchID, Location, Manager) values (457, '8686 Shelley Way', 892);
