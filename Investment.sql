3d4cc019ad7de1b69b9a268aba4cd7c08db483b1
easeghehey <98903704+easeghehey@users.noreply.github.com>
2022-06-26 15:57:02 -0400

    added more investments

--- Investment.sql
+++ Investment.sql
@@ -1,5 +1,5 @@
 create table Investment (
-	IID INT,
+	IID INT PRIMARY KEY,
 	Type VARCHAR(50),
 	Name VARCHAR(50),
 	Risk_Assessment INT
@@ -7,10 +7,18 @@ create table Investment (
 insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311203, 'Medical Specialities', 'AET', 8);
 insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311201, 'Railroads', 'RAIL', 33);
 insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311240, 'Computer Software: Programming, Data Processing', 'MOXC', 35);
-insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311278, 'n/a', 'VCLT', 91);
-insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311294, 'n/a', 'STAG^C', 42);
-insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311299, 'n/a', 'SAR', 64);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311278, 'Index Funds', 'VCLT', 91);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311294, 'Cryptocurrency', 'Bitcoin', 42);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311299, 'Cryptocurrency', 'Dogecoin', 64);
 insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311248, 'Automotive Aftermarket', 'WMAR', 64);
 insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311279, 'Diversified Commercial Services', 'CAI', 33);
 insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311291, 'Electric Utilities: Central', 'MGEE', 89);
 insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311295, 'Advertising', 'MDCA', 5);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311208, 'Index Funds', 'Fidelity ZERO Large Cap Index (FNILX)', 12);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311280, 'Computer Software: Programming, Data Processing', 'TUL', 15);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311013, 'Medical Specialities', 'pHarmA', 8);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311694, 'Cryptocurrency', 'Coinbase', 87);
+
+
+
+
