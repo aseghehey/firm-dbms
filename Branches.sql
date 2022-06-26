194d714e4ec41a6213b7f0a0088d9533fcf1c738
pratuln <95243563+pratuln@users.noreply.github.com>
2022-06-26 15:15:48 -0400

    Update Branches.sql
    Linked managers to branches

--- Branches.sql
+++ Branches.sql
@@ -3,9 +3,9 @@ create table Branches (
 	Location VARCHAR(50),
 	Manager INT
 );
-insert into Branches (BranchID, Location, Manager) values (457, '0 Debra Avenue', 846);
-insert into Branches (BranchID, Location, Manager) values (465, '292 Longview Trail', 836);
-insert into Branches (BranchID, Location, Manager) values (416, '8 Carioca Junction', 830);
-insert into Branches (BranchID, Location, Manager) values (495, '0 Jenna Court', 824);
-insert into Branches (BranchID, Location, Manager) values (424, '225 Commercial Alley', 883);
-insert into Branches (BranchID, Location, Manager) values (457, '8686 Shelley Way', 892);
+insert into Branches (BranchID, Location, Manager) values (457, '0 Debra Avenue', 837);
+insert into Branches (BranchID, Location, Manager) values (465, '292 Longview Trail', 800);
+insert into Branches (BranchID, Location, Manager) values (416, '8 Carioca Junction', 813);
+insert into Branches (BranchID, Location, Manager) values (495, '0 Jenna Court', 882);
+insert into Branches (BranchID, Location, Manager) values (424, '225 Commercial Alley', 886);
+insert into Branches (BranchID, Location, Manager) values (457, '8686 Shelley Way', 889);
