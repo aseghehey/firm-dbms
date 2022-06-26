f6f93c6d306bc4d5ca518f7e058d9ec73f2ecb22
pratuln <95243563+pratuln@users.noreply.github.com>
2022-06-26 15:24:37 -0400

    Update Investment.sql
    Fixed Investment ids to add beginning

--- Investment.sql
+++ Investment.sql
@@ -4,13 +4,13 @@ create table Investment (
 	Name VARCHAR(50),
 	Risk_Assessment INT
 );
-insert into Investment (IID, Type, Name, Risk_Assessment) values (203, 'Medical Specialities', 'AET', 8);
-insert into Investment (IID, Type, Name, Risk_Assessment) values (201, 'Railroads', 'RAIL', 33);
-insert into Investment (IID, Type, Name, Risk_Assessment) values (240, 'Computer Software: Programming, Data Processing', 'MOXC', 35);
-insert into Investment (IID, Type, Name, Risk_Assessment) values (278, 'n/a', 'VCLT', 91);
-insert into Investment (IID, Type, Name, Risk_Assessment) values (294, 'n/a', 'STAG^C', 42);
-insert into Investment (IID, Type, Name, Risk_Assessment) values (299, 'n/a', 'SAR', 64);
-insert into Investment (IID, Type, Name, Risk_Assessment) values (248, 'Automotive Aftermarket', 'WMAR', 64);
-insert into Investment (IID, Type, Name, Risk_Assessment) values (279, 'Diversified Commercial Services', 'CAI', 33);
-insert into Investment (IID, Type, Name, Risk_Assessment) values (291, 'Electric Utilities: Central', 'MGEE', 89);
-insert into Investment (IID, Type, Name, Risk_Assessment) values (295, 'Advertising', 'MDCA', 5);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311203, 'Medical Specialities', 'AET', 8);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311201, 'Railroads', 'RAIL', 33);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311240, 'Computer Software: Programming, Data Processing', 'MOXC', 35);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311278, 'n/a', 'VCLT', 91);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311294, 'n/a', 'STAG^C', 42);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311299, 'n/a', 'SAR', 64);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311248, 'Automotive Aftermarket', 'WMAR', 64);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311279, 'Diversified Commercial Services', 'CAI', 33);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311291, 'Electric Utilities: Central', 'MGEE', 89);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (00054311295, 'Advertising', 'MDCA', 5);
