e6aa6e6c41393759a4fa32537f3019c89bdb4512
pratuln <95243563+pratuln@users.noreply.github.com>
2022-06-26 15:05:06 -0400

    Add files via upload

---
+++ Investment.sql
@@ -0,0 +1,16 @@
+create table Investment (
+	IID INT,
+	Type VARCHAR(50),
+	Name VARCHAR(50),
+	Risk_Assessment INT
+);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (203, 'Medical Specialities', 'AET', 8);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (201, 'Railroads', 'RAIL', 33);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (240, 'Computer Software: Programming, Data Processing', 'MOXC', 35);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (278, 'n/a', 'VCLT', 91);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (294, 'n/a', 'STAG^C', 42);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (299, 'n/a', 'SAR', 64);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (248, 'Automotive Aftermarket', 'WMAR', 64);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (279, 'Diversified Commercial Services', 'CAI', 33);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (291, 'Electric Utilities: Central', 'MGEE', 89);
+insert into Investment (IID, Type, Name, Risk_Assessment) values (295, 'Advertising', 'MDCA', 5);
