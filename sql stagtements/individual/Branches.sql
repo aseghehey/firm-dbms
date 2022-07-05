8b5c531d91fe2f9dd969403c6ffa4edac8164065
pratuln <95243563+pratuln@users.noreply.github.com>
2022-07-05 14:39:08 -0400

    Fixed Branch num to match brokers' branches

--- sql stagtements/individual/Branches.sql
+++ sql stagtements/individual/Branches.sql
@@ -3,9 +3,9 @@ create table Branches (
 	Location VARCHAR(50),
 	Manager INT
 );
-insert into Branches (BranchID, Location, Manager) values (457, '0 Debra Avenue', 98445123889);
-insert into Branches (BranchID, Location, Manager) values (465, '292 Longview Trail', 98445123800);
-insert into Branches (BranchID, Location, Manager) values (416, '8 Carioca Junction', 98445123882);
-insert into Branches (BranchID, Location, Manager) values (495, '0 Jenna Court', 98445123813);
-insert into Branches (BranchID, Location, Manager) values (424, '225 Commercial Alley', 98445123886);
-insert into Branches (BranchID, Location, Manager) values (457, '8686 Shelley Way', 98445123837);
+insert into Branches (BranchID, Location, Manager) values (837, '0 Debra Avenue', 98445123889);
+insert into Branches (BranchID, Location, Manager) values (813, '292 Longview Trail', 98445123800);
+insert into Branches (BranchID, Location, Manager) values (800, '8 Carioca Junction', 98445123882);
+insert into Branches (BranchID, Location, Manager) values (882, '0 Jenna Court', 98445123813);
+insert into Branches (BranchID, Location, Manager) values (886, '225 Commercial Alley', 98445123886);
+insert into Branches (BranchID, Location, Manager) values (889, '8686 Shelley Way', 98445123837);
